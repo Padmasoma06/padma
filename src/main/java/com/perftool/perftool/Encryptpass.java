dae4dcd28b60b10ab26964647e01fd48845f06a0
Padmasoma06 <48563308+Padmasoma06@users.noreply.github.com>
2019-03-17 23:19:39 -0500

    Update Encryptpasss.java

--- src/main/java/com/perftool/perftool/Encryptpass.java
+++ src/main/java/com/perftool/perftool/Encryptpass.java
@@ -5,7 +5,7 @@ import org.apache.tomcat.util.codec.binary.Base64;
 public class Encryptpass {
 
 	public static void main(String[] args) {
-		String password="padma2280";
+		String password="XXXXXX";
 		String encrypted=new String(Base64.encodeBase64(password.getBytes()));
 		System.out.println("encrypted code is  "+encrypted);
 		String decrypted =new String(Base64.decodeBase64(encrypted));
